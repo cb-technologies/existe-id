2dffb362d16cd936b0b621dd03ed5d41eded4383
Johan  Mufuta <jmufuta2@illinois.edu>
2022-09-04 16:19:19 -0400

    feature-aws_setup: fixing typo in vars.tf

--- vars.tf
+++ vars.tf
@@ -37,7 +37,7 @@ variable "subnet1_cidr" {
 # Defining AMI
 variable "ami" {
   default = {
-    eu-west-1 = "ami-0ea3405d2d2522162"
+    eu-west-1 = "ami-0ea3405d2d2522162",
     us-east-1 = "ami-09d95fab7fff3776c"
   }
 }
@@ -50,4 +50,4 @@ variable "instancetype" {
 # Defining Master count
 variable "master_count" {
   default = 1
-}goo
+}
