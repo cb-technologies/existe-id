835a2741d8dfdaf574eecf28e36e513be75374a9
Johan  Mufuta <jmufuta2@illinois.edu>
2022-09-04 22:18:29 -0400

    feature-aws_setup: one vpc instead of multiple

--- main.tf
+++ main.tf
@@ -51,77 +51,29 @@ resource "aws_subnet" "demosubnet1" {
   }
 }
 
+
+data "aws_vpc" "default" {
+  default = true
+}
+
 # Creating Security Group
-resource "aws_security_group" "demosg" {
-  name        = "Demo Security Group"
-  description = "Demo Module"
-  vpc_id      = "${aws_vpc.demovpc.id}"
-
-  # Inbound Rules
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
+resource "aws_security_group" "existvpc" {
+  vpc_id      = "${data.aws_vpc.default.id}"
+  name        = "existvpc"
+  description = "Allow all inbound for Postgres"
+ingress {
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  # HTTPS access from anywhere
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Splunk default port
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Replication Port
-  ingress {
-    from_port   = 8089
-    to_port     = 8089
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Management Port
-  ingress {
-    from_port   = 4598
-    to_port     = 4598
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Ingestion Port
-  ingress {
-    from_port   = 9997
-    to_port     = 9997
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound Rules
-  # Internet access to anywhere
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "random_string" "exist-db-password" {
+  length  = 32
+  upper   = true
+  number  = true
+  special = false
 }
 
 # Postgres RDS instance
@@ -134,7 +86,7 @@ resource "aws_db_instance" "exist-postgres" {
   engine_version         = "13.4"
   skip_final_snapshot    = true
   publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.demosg.id]
+  vpc_security_group_ids = [aws_security_group.existvpc.id]
   username               = "existuser"
   password               = "random_string.exist-db-password.result}"
 }
