3ca1cdb5f55217bcd2b740dcec9f4e6863eaeee3
Johan  Mufuta <jmufuta2@illinois.edu>
2022-09-05 13:06:17 -0400

    feature-aws_setup: fixing default vpc recereating postgresql

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 # Creating VPC
-resource "aws_vpc" "exist_vpc" {
+resource "aws_default_vpc" "exist_vpc" {
   cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
 
@@ -18,19 +18,19 @@ resource "aws_vpc" "exist_vpc" {
 
 # Creating Internet Gateway
 resource "aws_internet_gateway" "existgateway" {
-  vpc_id = "${aws_vpc.exist_vpc.id}"
+  vpc_id = "${aws_default_vpc.exist_vpc.id}"
 }
 
 # Grant the internet access to VPC by updating its main route table
 resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.exist_vpc.main_route_table_id}"
+  route_table_id         = "${aws_default_vpc.exist_vpc.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.existgateway.id}"
 }
 
 # Creating 1st subnet
 resource "aws_subnet" "demosubnet" {
-  vpc_id                  = "${aws_vpc.exist_vpc.id}"
+  vpc_id                  = "${aws_default_vpc.exist_vpc.id}"
   cidr_block             = "${var.subnet_cidr}"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
@@ -42,6 +42,7 @@ resource "aws_subnet" "demosubnet" {
 
 # Creating Security Group
 resource "aws_security_group" "existvpc" {
+  vpc_id      = "${data.aws_vpc.default.id}"
   name        = "existvpc"
   description = "Allow all inbound for Postgres"
 ingress {
@@ -52,9 +53,9 @@ ingress {
   }
 }
 
-//data "aws_vpc" "default" {
-//  default = true
-//}
+data "aws_vpc" "default" {
+  default = true
+}
 resource "random_string" "exist-db-password" {
   length  = 32
   upper   = true
