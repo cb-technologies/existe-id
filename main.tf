e373ece91a233fed7f3d591221846c12c7b2d790
Johan  Mufuta <jmufuta2@illinois.edu>
2022-09-04 21:50:42 -0400

    feature-aws_setup: fixing wrong path file and creating exist-postgres db

--- main.tf
+++ main.tf
@@ -124,77 +124,17 @@ resource "aws_security_group" "demosg" {
   }
 }
 
-# Creating key pair
-resource "aws_key_pair" "demokey" {
-  key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key)}"
-}
-
-# Creating EC2 Instance
-resource "aws_instance" "demoinstance" {
-
-  # AMI based on region
-  ami = "${lookup(var.ami, var.aws_region)}"
-
-  # Launching instance into subnet
-  subnet_id = "${aws_subnet.demosubnet.id}"
-
-  # Instance type
-  instance_type = "${var.instancetype}"
-
-  # Count of instance
-  count= "${var.master_count}"
-
-  # SSH key that we have generated above for connection
-  key_name = "${aws_key_pair.demokey.id}"
-
-  # Attaching security group to our instance
-  vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
-
-  # Attaching Tag to Instance
-  tags = {
-    Name = "Search-Head-${count.index + 1}"
-  }
-
-  # Root Block Storage
-  root_block_device {
-    volume_size = "40"
-    volume_type = "standard"
-  }
-
-  #EBS Block Storage
-  ebs_block_device {
-    device_name = "/dev/sdb"
-    volume_size = "80"
-    volume_type = "standard"
-    delete_on_termination = false
-  }
-
-  # SSH into instance
-  connection {
-
-    # Host name
-    host = self.public_ip
-    # The default username for our AMI
-    user = "ec2-user"
-    # Private key for connection
-    private_key = "${file(var.private_key)}"
-    # Type of connection
-    type = "ssh"
-  }
-
-  # Installing splunk on newly created instance
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chkconfig docker on",
-      "sudo yum install -y git",
-      "sudo chmod 666 /var/run/docker.sock",
-      "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"
-  ]
- }
+# Postgres RDS instance
+resource "aws_db_instance" "exist-postgres" {
+  identifier             = "exist-identifier"
+  name                   = "existdb"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "12.5"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.demosg.id]
+  username               = "existuser"
+  password               = "random_string.exist-db-password.result}"
 }
--- vars.tf
+++ vars.tf
@@ -1,18 +1,3 @@
-# Defining Public Key
-variable "public_key" {
-  default = "tests.pub"
-}
-
-# Defining Private Key
-variable "private_key" {
-  default = "tests.pem"
-}
-
-# Definign Key Name for connection
-variable "key_name" {
-  default = "tests"
-  description = "Desired name of AWS key pair"
-}
 
 # Defining Region
 variable "aws_region" {
@@ -34,17 +19,6 @@ variable "subnet1_cidr" {
   default = "10.0.2.0/24"
 }
 
-# Defining AMI
-variable "ami" {
-  default = {
-    us-east-1 = "ami-09d95fab7fff3776c"
-  }
-}
-
-# Defining Instace Type
-variable "instancetype" {
-  default = "t2.medium"
-}
 
 # Defining Master count
 variable "master_count" {
