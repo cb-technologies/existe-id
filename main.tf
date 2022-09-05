cc3bfaf6876600ece161525a3f3b395731d70d56
Johan  Mufuta <jmufuta2@illinois.edu>
2022-09-05 02:45:52 -0400

    feature-aws_setup: recereating postgresql

--- main.tf
+++ main.tf
@@ -36,19 +36,7 @@ resource "aws_subnet" "demosubnet" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Demo subnet"
-  }
-}
-
-# Creating 2nd subnet
-resource "aws_subnet" "demosubnet1" {
-  vpc_id                  = "${aws_vpc.exist_vpc.id}"
-  cidr_block             = "${var.subnet1_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Demo subnet 1"
+    Name = "Exist subnet"
   }
 }
 
