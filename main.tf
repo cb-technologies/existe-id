2b771a3f90d93612eaebb8515ae17b0c8edbad71
WallDestroyer <johan.mufuta@gmail.com>
2022-09-06 23:15:54 -0400

    Feature aws setup (#11)
    * feature-aws_setup: wrong yaml file
    
    * feature-aws_setup: fixing wrong path file and creating exist-postgres db
    
    * feature-aws_setup: fixing wrong path file and creating exist-postgres db
    
    * feature-aws_setup: using db.t3.micro for postgres sql instance
    
    * feature-aws_setup: one vpc instead of multiple
    
    * feature-aws_setup: remove auto terraform apply
    
    * feature-aws_setup: terraform destroy
    
    * feature-aws_setup: terraform destroy
    
    * feature-aws_setup: terraform destroy
    
    * feature-aws_setup: terraform destroy
    
    * feature-aws_setup: terraform destroy
    
    * feature-aws_setup: recereating postgresql
    
    * feature-aws_setup: recereating postgresql
    
    * feature-aws_setup: recereating postgresql
    
    * feature-aws_setup: recereating postgresql
    
    * feature-aws_setup: recereating postgresql
    
    * feature-aws_setup: recereating postgresql
    
    * feature-aws_setup: recereating postgresql
    
    * feature-aws_setup: fixing default vpc recereating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: using aws security group rule to prevent recreating vpc
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: fixing default vpc recreating postgresql
    
    * feature-aws_setup: importing resources
    
    * feature-aws_setup: importing resources
    
    * feature-aws_setup: importing resources
    
    * feature-aws_setup: importing database
    
    Co-authored-by: Johan  Mufuta <jmufuta2@illinois.edu>

--- main.tf
+++ main.tf
@@ -5,50 +5,15 @@ provider "aws" {
   region     = "${var.aws_region}"
 }
 
-# Creating VPC
-resource "aws_vpc" "exist_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "Exist VPC"
-  }
-}
-
-
-# Creating Internet Gateway
-resource "aws_internet_gateway" "existgateway" {
-  vpc_id = "${aws_vpc.exist_vpc.id}"
-}
-
-# Grant the internet access to VPC by updating its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.exist_vpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.existgateway.id}"
-}
-
-# Creating 1st subnet
-resource "aws_subnet" "demosubnet" {
-  vpc_id                  = "${aws_vpc.exist_vpc.id}"
-  cidr_block             = "${var.subnet_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Exist subnet"
-  }
-}
-
+# Retreiving VPC
 
 data "aws_vpc" "default" {
   default = true
 }
-
 # Creating Security Group
-resource "aws_security_group" "existvpc" {
+resource "aws_security_group" "exist_security_group" {
   vpc_id      = "${data.aws_vpc.default.id}"
-  name        = "existvpc"
+  name        = "exist"
   description = "Allow all inbound for Postgres"
 ingress {
     from_port   = 5432
@@ -57,13 +22,13 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 resource "random_string" "exist-db-password" {
   length  = 32
   upper   = true
   number  = true
   special = false
 }
+
 
 # Postgres RDS instance
 resource "aws_db_instance" "exist-postgres" {
@@ -75,7 +40,7 @@ resource "aws_db_instance" "exist-postgres" {
   engine_version         = "13.4"
   skip_final_snapshot    = true
   publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.existvpc.id]
+  vpc_security_group_ids = [aws_security_group.exist_security_group.id]
   username               = "existuser"
   password               = "random_string.exist-db-password.result}"
 }
