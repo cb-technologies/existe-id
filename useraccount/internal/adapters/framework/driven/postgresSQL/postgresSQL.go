9bf32a9a21c90859f1e647a09f17ff67fa2b6def
nicolasnk <nkierenicolas@gmail.com>
2022-09-14 23:45:40 -0700

    Implemented schema definition of our data in protobuffer and started to define interface in ports and their adapeters. Could not generate go code from the proto files because I need to fix an issue with the protoc compiler that I have installed on my computer. Will be done in the upcoming days.

---
+++ useraccount/internal/adapters/framework/driven/postgresSQL/postgresSQL.go
@@ -0,0 +1,9 @@
+package postgresSQL
+
+//  Johan will probably start working here
+
+// the adapter that will implement the interface defined in ports
+type Adapter struct {
+}
+
+
--- useraccount/internal/ports/drivenframework.go
+++ useraccount/internal/ports/drivenframework.go
@@ -1,1 +1,10 @@
 package ports
+
+type PostgresSQLPort interface {
+	OpenDBConnection()
+	CloseDBConnection()
+	AddNewPersonInfo()
+	DeletePersonInfo()
+	UpdatePersonInfo()
+	FindPersonInfo()
+}
