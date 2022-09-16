0b86d9125e7a82c95ce9f6bf55b14847895a3d7f
nicolasnk <nkierenicolas@gmail.com>
2022-09-15 23:14:23 -0700

    Added some ports for the Postgres database and Implemented some of the Postgres function

--- useraccount/internal/adapters/application/api/api.go
+++ useraccount/internal/adapters/application/api/api.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	"github.com/cb-technologies/existe-id/useraccount/useraccount/internal/adapters/framework/driver/grpc/pb"
+	"github.com/cb-technologies/existe-id/useraccount/useraccount/internal/ports"
+)
+
+type Adapter struct {
+	db ports.PostgresSQLPort
+}
+
+func NewAdapter(db ports.PostgresSQLPort) *Adapter {
+	return &Adapter{db: db}
+}
+
+func (adapter Adapter) AddNewPersonInfo(personInfo pb.PersonInfo) error {
+	// make the call to the db
+
+	return nil
+}
+
+func (adapter Adapter) UpdatePersonInfo(parameters pb.EditPersonInfoParameters) error {
+	// make a call to the DB
+
+	return nil
+}
+
+func (adapter Adapter) FindPersonInfo(uuid pb.UUID) (pb.PersonInfo, error) {
+
+	return pb.PersonInfo{}, nil
+}
--- useraccount/internal/adapters/framework/driven/postgresSQL/postgresSQL.go
+++ useraccount/internal/adapters/framework/driven/postgresSQL/postgresSQL.go
@@ -1,29 +1,59 @@
 package postgresSQL
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
 //  Johan will probably start working here
 
 // the adapter that will implement the interface defined in ports
 type Adapter struct {
+	db *sql.DB
 }
 
-func NewAdapter() *Adapter {
-	// TODO: modify 
-	return &Adapter{}
+const (
+	host     = " localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "xx ="
+	dbname   = "exist-id"
+)
 
+func NewAdapter() (*Adapter, error) {
+	postgresDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", postgresDbInfo)
+
+	if err != nil {
+		panic(err) // have to know what to do in this case
+	}
+
+	defer db.Close()
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &Adapter{db: db}, nil
 }
 
-func (a Adapter) OpenDBConnection() {
+func (adapter Adapter) CloseDBConnection() {
+	err := adapter.db.Close()
+	if err != nil {
+		log.Fatalf("db close failure: %v", err)
+	}
+}
+func (adapter Adapter) AddNewPersonInfo() {
 	// TODO: implement
 }
-func (a Adapter) CloseDBConnection() {
+func (adapter Adapter) UpdatePersonInfo() {
 	// TODO: implement
 }
-func (a Adapter) AddNewPersonInfo() {
-	// TODO: implement
-}
-func (a Adapter) UpdatePersonInfo() {
-	// TODO: implement
-}
-func (a Adapter) FindPersonInfo() {
+func (adapter Adapter) FindPersonInfo() {
 	// TODO: implement
 }
