291779c48de9946a571125cff307153fa3de4e5e
Johan  Mufuta <jmufuta2@illinois.edu>
2022-09-20 00:30:23 -0400

    feature-aws_setup: adding cluster

--- main.tf
+++ main.tf
@@ -29,18 +29,38 @@ resource "random_string" "exist-db-password" {
   special = false
 }
 
+# Create Postgres Cluster
+
+resource "aws_rds_cluster" exist_cluster {
+  cluster_identifier = "aurora-exist-cluster"
+  engine = "aurora-postgresql"
+  engine_version = "13.4"
+  database_name = "existdb"
+  master_username = "postgres"
+  master_password = "exist_password"
+  iam_database_authentication_enabled = true
+  vpc_security_group_ids = [aws_security_group.exist_security_group.id]
+}
 
 # Postgres RDS instance
 resource "aws_db_instance" "exist-postgres" {
+  count                  = 1
   identifier             = "exist-identifier"
+  cluster_identifer      =  aws_rds_cluster.exist_cluster.id
   name                   = "existdb"
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
-  engine_version         = "13.4"
+  engine_version         = aws_rds_cluster.exist_cluster.engine_version
   skip_final_snapshot    = true
   publicly_accessible    = true
   vpc_security_group_ids = [aws_security_group.exist_security_group.id]
   username               = "existuser"
-  password               = "random_string.exist-db-password.result}"
 }
+
+//data "aws_caller_identity" "current_exist"{}
+//
+//variable "dbuser"{
+//  type    = string
+//  default = "dbuser"
+//}
