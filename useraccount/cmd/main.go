dfb30e90f1573bd333e9b987d940f6784acc7d74
Johan  Mufuta <jmufuta2@illinois.edu>
2022-09-21 23:13:15 -0400

    feature-aws_setup: starting testing db connection

--- useraccount/cmd/main.go
+++ useraccount/cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-	import "useraccount/internal/adapters/framework/driven/postgreSQL"
+import "github.com/cb-technologies/existe-id/useraccount/internal/adapters/framework/driven/postgreSQL"
 
 func main() {
 	fmt.Println("Existe ID")
