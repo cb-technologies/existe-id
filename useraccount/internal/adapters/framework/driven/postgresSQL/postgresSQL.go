50da0c122db3adc3d590c1968ec3116b3ded316b
masanka2 <masanka2@illinois.edu>
2022-09-22 22:44:28 -0700

    Database connection

--- useraccount/internal/adapters/framework/driven/postgresSQL/postgresSQL.go
+++ useraccount/internal/adapters/framework/driven/postgresSQL/postgresSQL.go
@@ -4,10 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"context"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/feature/rds/auth"
 
+	_ "github.com/lib/pq"
 )
 
 //  Johan will probably start working here
@@ -18,41 +16,29 @@ type Adapter struct {
 }
 
 const (
-	dbPort      = 5432
-	dbUser      = "dbuser"
-	dbHost 		= "exist-identifier.cprbzqerfjiq.us-east-1.rds.amazonaws.com"
-	dbName      = "existdb"
-	region		= "us-east-1"
+	dbPort     = 5432
+	dbUser     = "postgres"
+	dbHost     = "exist-identifier.cprbzqerfjiq.us-east-1.rds.amazonaws.com"
+	dbName     = "existdb"
+	region     = "us-east-1"
+	dbPassword = "test1234"
 )
 
 func NewAdapter() (*Adapter, error) {
-	var dbEndpoint string = fmt.Sprintf("%s:%d", dbHost, dbPort)
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-   if err != nil {
-	   panic("configuration error: " + err.Error())
-   }
-
-   authenticationToken, err := auth.BuildAuthToken(
-	context.TODO(), dbEndpoint, region, dbUser, cfg.Credentials)
-	if err != nil {
-		panic("failed to create authentication token: " + err.Error())
-	}
 
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
-	   dbHost, dbPort, dbUser, authenticationToken, dbName,
-   )
-
-
+		dbHost, dbPort, dbUser, dbPassword, dbName,
+	)
 
 	db, err := sql.Open("postgres", dsn)
-   if err != nil {
-	   panic(err)
-   }
+	if err != nil {
+		panic(err)
+	}
 
-   err = db.Ping()
-   if err != nil {
-	   panic(err)
-   }
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
 	return &Adapter{db: db}, nil
 }
