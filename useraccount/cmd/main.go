53c0e3bf1de87ca697d564d6090254cadb11b436
Nathan Tuala <nathan@Nathans-MacBook-Pro.local>
2022-09-28 19:32:23 -0500

    Working on UUID

--- useraccount/cmd/main.go
+++ useraccount/cmd/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-
+	"github.com/google/uuid"
 	"github.com/cb-technologies/existe-id/useraccount/useraccount/internal/adapters/framework/driven/postgresSQL"
 	//"github.com/cb-technologies/existe-id/useraccount/useraccount/internal/adapters/framework/driver/grpc/pb"
-	"github.com/cb-technologies/existe-id/useraccount/useraccount/internal/entity/db"
-	"github.com/google/uuid"
 )
 
 func main() {
@@ -22,55 +20,57 @@ func main() {
 	fmt.Println("Connection succesful!")
 
 	// Trying to mimic a request just to see if the GORM code is working
-	namesTest := db.NamesModel{
-		Nom:         "Bingoto",
-		Prenom:      "Areely",
-		MiddleNames: []string{"Bamanissa"},
-	}
-	biometricsTest := db.BiometricModel{
-		Photos:      []uint8{1, 2},
-		FingerPrint: []uint8{2, 3},
-	}
+	// namesTest := db.NamesModel{
+	// 	Nom:         "Bingoto",
+	// 	Prenom:      "Areely",
+	// 	MiddleNames: []string{"Bamanissa"},
+	// }
+	// biometricsTest := db.BiometricModel{
+	// 	Photos:      []uint8{1, 2},
+	// 	FingerPrint: []uint8{2, 3},
+	// }
 
-	addressTest := db.AddressModel{
-		Number:   1,
-		Avenue:   "Nicolas",
-		Quartier: "Santa Clara",
-	}
+	// addressTest := db.AddressModel{
+	// 	Number:   1,
+	// 	Avenue:   "Nicolas",
+	// 	Quartier: "Santa Clara",
+	// }
 
-	originTest := db.OriginModel{
-		Province: []string{"Bandundu"},
-	}
+	// originTest := db.OriginModel{
+	// 	Province: []string{"Bandundu"},
+	// }
 
-	phenotypeTest := db.PhenotypeModel{
-		EyeColor: "blue",
-	}
+	// phenotypeTest := db.PhenotypeModel{
+	// 	EyeColor: "blue",
+	// }
 
-	dateOfBirthTest := db.DateOfBirthModel{
-		Day:   "Monday",
-		Month: "February",
-		Year:  "2002",
-	}
-	my_UUID := uuid.New()
+	// dateOfBirthTest := db.DateOfBirthModel{
+	// 	Day:   "Monday",
+	// 	Month: "February",
+	// 	Year:  "2002",
+	// }
+	// my_UUID := uuid.New()
 
-	personTest := db.PersonInfoModel{
-		Names:       namesTest,
-		Biometrics:  biometricsTest,
-		Address:     addressTest,
-		Origins:     originTest,
-		Phenotypes:  phenotypeTest,
-		DateOfBirth: dateOfBirthTest,
-		UUID:        my_UUID,
-	}
+	// personTest := db.PersonInfoModel{
+	// 	Names:       namesTest,
+	// 	Biometrics:  biometricsTest,
+	// 	Address:     addressTest,
+	// 	Origins:     originTest,
+	// 	Phenotypes:  phenotypeTest,
+	// 	DateOfBirth: dateOfBirthTest,
+	// 	UUID:        my_UUID,
+	// }
 
-	err = postgres.AddNewPersonInfo(&personTest)
-
+	//err = postgres.AddNewPersonInfo(&personTest)
+	my_id uuid.UUID := "915aa63c-68e8-4c0f-b884-033b2673ab14";
+	postgres.UpdatePersonInfo()
 	if err != nil {
 		fmt.Println("Test Failed! A demain faut dormir")
 	} else {
-		fmt.Println("Person created successfully with UUID", my_UUID)
+		fmt.Println("Person created successfully with UUID")
 	}
 
+	// 915aa63c-68e8-4c0f-b884-033b2673ab14
 	postgres.CloseDBConnection()
 
 }
