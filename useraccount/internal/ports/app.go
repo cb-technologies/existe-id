c68e502f52cf9cd4f9c3f9e0535d2824720c5f5f
Johan  Mufuta <jmufuta2@illinois.edu>
2022-11-12 21:02:04 -0500

    merging retreiveUserInfo to main branch

--- useraccount/internal/ports/app.go
+++ useraccount/internal/ports/app.go
@@ -8,6 +8,7 @@ type APIPorts interface {
 	AddNewPerson(personInfo *pb.PersonInfoRequest) error
 	UpdatePersonInfo(parameters *pb.EditPersonInfoParameters) error
 	FindPersonInfo(nationalID *pb.NationalIDNumber) (*pb.PersonInfoResponse, error)
+	RetreiveUserBasedOnField(names *pb.Names, dateOfBirth *pb.DateOfBirth) (*pb.PersonInfoResponse, error)
 	GenerateNationalID() (*string, error)
 	SignInAgent(agentSignInInfo *pb.AgentSignInInfo) error
 	SignUpAgent(agentSignUpInfo *pb.AgentInfo) error
