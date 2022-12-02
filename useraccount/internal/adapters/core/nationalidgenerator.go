628cec093413ecc137e271acece442a103ec273a
Johan  Mufuta <jmufuta2@illinois.edu>
2022-12-02 01:11:05 -0500

    generated a 9 digit code right after Province abbreviaton

--- useraccount/internal/adapters/core/nationalidgenerator.go
+++ useraccount/internal/adapters/core/nationalidgenerator.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"github.com/cb-technologies/existe-id/useraccount/useraccount/internal/adapters/framework/driver/grpc/pb"
 	"math/rand"
+	"time"
 )
 
 func (adapter Adapter) GenerateNationalID(personInfo *pb.PersonInfoRequest) (*string) {
@@ -45,14 +46,15 @@ func generateUniqueNationalID(personInfo *pb.PersonInfoRequest) (*string) {
 	
 	for key, _ := range congo_provinces {
 		if(key == personInfo.Address.Province){
-			province_abb := congo_provinces[key]
-			log.Printf("The abbreviation of %s is %s\n", personInfo.Address.Province, province_abb)
+			province_abb = congo_provinces[key]
+			log.Printf("The abbreviation of %s is %s\n",personInfo.Address.Province, province_abb)
 		}
 	}
-	log.Printf("The abbreviation of %s is %s\n", personInfo.Address.Province, province_abb)
-
-	id_number := 100000000 + rand.Intn(999999999-100000000)
-	nationalIdStr := strconv.FormatInt(int64(id_number), 16)
+	log.Printf("The abbreviation of %s is %s\n",personInfo.Address.Province, province_abb)
+	rand.Seed(time.Now().UnixNano())
+	id_number := 0 + rand.Intn(999999999-100000000)
+	nationalIdStr := strconv.Itoa(id_number)
+	log.Printf(nationalIdStr)
 	final_id := province_abb + "-" + nationalIdStr
 	return &final_id
 }
